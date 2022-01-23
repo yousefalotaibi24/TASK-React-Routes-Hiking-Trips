6744d4169ee9440823b48fd59bbf6759e3e52be8
YanalShmilan <84308096+YanalShmilan@users.noreply.github.com>
2022-01-23 19:39:42 +0200

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Nav() {
   return (
     <nav
-      className="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top"
+      className="navbar navbar-expand-lg bg-secondary text-uppercase "
       id="mainNav"
     >
       <div className="container">
